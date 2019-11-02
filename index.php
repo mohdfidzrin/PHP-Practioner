b1f1387360b079b3742ad2e114e1342acd9d414e
Mohd Fidzrin Bin Mohd Hamizaz <mohdfidzrin@plenitude-solutions.com>
2019-11-03 01:44:42 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+$names = [
+    'Sulong' => 'Rin',
+    'Tengah' => 'Fidzrin',
+    'Bongsu' => 'Fid',
+	'Last' => 'Ye'
+];
+
+$names['baru'] = 'Rein';
+
+var_dump($names);
+
+require 'index.view.php';
