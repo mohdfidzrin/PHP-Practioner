496bf5e1439ed2b97940da2fd23ad8ebf7da72b9
Mohd Fidzrin Bin Mohd Hamizaz <mohdfidzrin@plenitude-solutions.com>
2019-11-03 17:30:24 +0800

    new classes functions

--- Classes/index.php
+++ Classes/index.php
@@ -10,6 +10,29 @@ class Task {
         // Automatically triggered on instantiation
         $this->description = $description;
     }
+
+    /**
+     * @param bool $completed
+     */
+    public function setCompleted($completed)
+    {
+        $this->completed = $completed;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isCompleted()
+    {
+        return $this->completed;
+    }
+
 }
 
-$task = new Task('Go to the store');
+$tasks = [
+    new Task('Go to the store'),
+    new Task('Fnish my screencast'),
+    new Task('Clean up')
+];
+
+require 'index.view.php';
